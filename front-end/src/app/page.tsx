382fb1b90b6dc2fe03bdd20f34cfbc8776e8bdb3
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2024-01-19 12:17:36 +0600

    add form, decode and encode request to frontend part

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,24 +1,88 @@
 'use client';
 import styles from './page.module.css'
+import axiosApi from "@/axiosApi";
+import {useState} from "react";
+import {useMutation, useQuery, useQueryClient, UseQueryResult} from "@tanstack/react-query";
+import {act} from "react-dom/test-utils";
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    decode: '',
+    encode: '',
+    password: '',
+  });
 
-  let productArea: React.ReactNode = <p>Loading</p>;
+  const encodeMessageReq  = useMutation( {
+    mutationFn: async (formData: {[key: string]: string}) => {
+      setLoading(true);
+      try {
+        const response = await axiosApi.post('/encode/' + formData.encode, {password: formData.password});
+        setForm((prev) => ({
+          ...prev,
+          decode: response.data,
+          encode: '',
+        }));
+      } catch (e) {
+        console.error(e);
+      }
+
+      setLoading(false);
+    }
+  });
+
+  const decodeMessageReq  = useMutation( {
+    mutationFn: async (formData: {[key: string]: string}) => {
+      setLoading(true);
+      try {
+        const response = await axiosApi.post('/decode/' + formData.decode, {password: formData.password});
+        setForm((prev) => ({
+          ...prev,
+          encode: response.data,
+          decode: '',
+        }));
+      }catch (e) {
+        console.error(e);
+      }
+
+      setLoading(false);
+    }
+  });
+
+  const changeForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+
+  const handleClick = async (action: string) => {
+
+    if (action === 'encode') {
+      await encodeMessageReq.mutateAsync(form);
+    } else {
+      await decodeMessageReq.mutateAsync(form);
+    }
+
+  };
 
 
   return (
     <main className={styles.main}>
-      <form>
-        <input type="text" name="encode"/>
-        <button type="button">Encode</button>
-        <hr/>
-        Password: <input type="password" name="password"/>
-        <hr/>
-        <input type="text" name="decode"/>
-        <button type="button">Decode</button>
+      {loading ? <p>Loading</p> :
 
-      </form>
-        {productArea}
+          <form>
+            <input type="text" name="encode" value={form.encode} onChange={changeForm}/>
+            <button type="button" onClick={() => handleClick('encode')}>Encode</button>
+            <hr/>
+            Password: <input type="password" name="password" value={form.password} onChange={changeForm}/>
+            <hr/>
+            <input type="text" name="decode" value={form.decode} onChange={changeForm}/>
+            <button type="button" onClick={() => handleClick('decode')}>Decode</button>
+          </form>
+      }
+
     </main>
   )
 };
