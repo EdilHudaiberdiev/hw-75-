b22fde6ad08de46962483dd283d746381ac378ef
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2024-01-19 09:53:57 +0600

    Added API from hw-73

---
+++ api/app.ts
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const Vigenere = require('caesar-salad').Vigenere;
+const password = '2835';
+
+app.get('/', (req, res) => {
+    res.send('Task 2');
+});
+
+app.get('/encode/:name', (req, res) => {
+    res.send(Vigenere.Cipher(password).crypt(req.params.name));
+});
+
+app.get('/decode/:name', (req, res) => {
+    res.send(Vigenere.Decipher(password).crypt(req.params.name));
+});
+
+app.listen(port, () => {
+    console.log('Server online on ' + port + ' port+');
+});
